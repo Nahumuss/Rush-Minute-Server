eff922e8f20c0e723e1c80b9c455ce9af7879df5
Nahumus <itaynahum2003@gmail.com>
2021-04-18 23:27:25 +0300

    made it not crash

--- dummy_client.py
+++ dummy_client.py
@@ -1,9 +1,18 @@
 from socket import socket
+import threading
+
+def read():
+    while True:
+        message = client_socket.recv(1024)
+        if not message:
+            break
+        print(message.decode())
 
 client_socket = socket()
 client_socket.connect(('127.0.0.1', 5635))
+t = threading.Thread(target=read)
+t.start()
 
 while True:
     message = input()
     client_socket.send(message.encode())
-    print(client_socket.recv(1024).decode(encoding='utf-8'))
